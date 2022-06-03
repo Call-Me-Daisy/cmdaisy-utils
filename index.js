bb13c87d168919ca7e74c6bf2cb1dd86dff038f8
TDCalverley99 <TDCalverley99@gmail.com>
2022-06-03 21:17:29 +0100

    Reimplemented automated pack & unpack functions

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const fs = require("fs");
 //--------------------------------------------------------------------CONSTANTS
-const out = {};
-const src = "./source/"
-//--------------------------------------------------------------------MAIN
-fs.readdir(src, (dir_e, files) => {
-	if (dir_e) { throw dir_e; }
-
-	for (const fileName of files) {
-		const [root, ext] = fileName.split(".");
-		ext === "js" && (out[root] = require(src + root));
+const exp = {
+	unpack() {
+		const out = [];
+		for (const subModule of arguments) {out.push(this[subModule]);}
+		return out;
 	}
-});
+};
+const src = require("path").join(__dirname, "source");
+//--------------------------------------------------------------------MAIN
+for (const fileName of require("fs").readdirSync(src)) {
+	const [root, ext] = fileName.split(".");
+	ext === "js" && (exp[root] = require(src + "/" + root));
+}
 //--------------------------------------------------------------------FINALIZE
-module.exports = out;
+module.exports = exp;
