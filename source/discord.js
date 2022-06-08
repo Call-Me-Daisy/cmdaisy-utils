73725582e43394c4928824bbfa35ac8b8509d27f
TDCalverley99 <TDCalverley99@gmail.com>
2022-06-08 12:07:02 +0100

    Discord: Added async bot.fetchChannel that bypasses cache

--- source/discord.js
+++ source/discord.js
@@ -1,11 +1,18 @@
 const {Client} = require("discord.js");
 //--------------------------------------------------------------------BOT
 class DiscordBot extends Client {
-	fetchChannel(_channelId) {
+	fetchCachedChannel(_channelId) {
 		return this.channels.cache.get(_channelId);
 	}
-	fetchMessage(_channelId, _messageId) {
-		return this.fetchChannel(_channelId).messages.fetch(_messageId);
+	async fetchChannel(_channelId) {
+		return this.channels.fetch(_channelId);
+	}
+
+	async fetchMessage(_channelId, _messageId, _channelIsCached = true) {
+		return (_channelIsCached)
+			? this.fetchCachedChannel(_channelId).messages.fetch(_messageId)
+			: this.fetchChannel(_channelId).then((_channel) => {return _channel.messages.fetch(_messageId)})
+		;
 	}
 	fetchReference(_msg) {
 		return _msg.reference !== null && this.fetchMessage(_msg.reference.channelId, _msg.reference.messageId);
